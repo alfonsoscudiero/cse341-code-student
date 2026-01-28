912bad0f4fa32687d123c9d60b1e6d28792b8fd1
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-01-28 16:01:00 -0700

    Fix Swagger annotations to use swagger-autogen syntax

--- routes/temple.js
+++ routes/temple.js
@@ -1,62 +1,24 @@
 const routes = require('express').Router();
 const temples = require('../controllers/temple.js');
 
-/**
- * @swagger
- * /temples:
- *   get:
- *     summary: Get all temples
- *     description: Returns a list of all temples
- *     responses:
- *       200:
- *         description: Successful response
- *       500:
- *         description: Internal server error
- */
-routes.get('/', temples.findAll);
 
-/**
- * @swagger
- * /temples/{temple_id}:
- *   get:
- *     summary: Get a temple by ID
- *     parameters:
- *       - in: path
- *         name: temple_id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Temple found
- *       404:
- *         description: Temple not found
- *       500:
- *         description: Internal server error
- */
-routes.get('/:temple_id', temples.findOne);
+routes.get('/', 
+  /* #swagger.summary = 'Get all temples' */
+  /* #swagger.description = 'Returns a list of all temples' */
+  temples.findAll
+);
 
-routes.get('/:temple_id', temples.findOne);
 
-/**
- * @swagger
- * /temples:
- *   post:
- *     summary: Create a new temple
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *     responses:
- *       201:
- *         description: Temple created successfully
- *       400:
- *         description: Invalid input
- *       500:
- *         description: Internal server error
- */
-routes.post('/', temples.create);
+routes.get('/:temple_id', 
+  /* #swagger.summary = 'Get a temple by ID' */
+  /* #swagger.description = 'Returns a single temple matching the provided temple ID' */
+  temples.findOne
+);
+
+routes.post('/', 
+  /* #swagger.summary = 'Create a new temple' */
+  /* #swagger.description = 'Creates a new temple and stores it in the database' */
+  temples.create
+);
 
 module.exports = routes;
